b031df432251b8f617f0104acabc0ab6de80a5b5
Sebastian Lamm <seba.lamm@googlemail.com>
2018-07-05 10:47:37 +0200

    Added seed for BA.

--- include/generators/barabassi/barabassi.h
+++ include/generators/barabassi/barabassi.h
@@ -84,7 +84,7 @@ class Barabassi {
         SInt r = 2 * (v * min_degree_ + i) + 1;
         do {
           // compute hash h(r)
-          SInt hash = sampling::Spooky::hash(r);
+          SInt hash = sampling::Spooky::hash(config_.seed + r);
           r = hash % r;
         } while (r % 2 == 1);
         SInt w = r / total_degree_;
