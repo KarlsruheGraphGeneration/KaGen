bca8e53a85a2d1fb89ffcd088026e38b604e26f1
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-04-29 10:26:53 +0200

    Fix unused functions warning

--- kagen/generators/kronecker/kronecker.cpp
+++ kagen/generators/kronecker/kronecker.cpp
@@ -35,51 +35,6 @@
 /* #define SPK_NOISE_LEVEL 1000 -- in INITIATOR_DENOMINATOR units */
 
 namespace kagen {
-
-#ifdef GENERATOR_USE_PACKED_EDGE_TYPE
-
-typedef struct packed_edge {
-    uint32_t v0_low;
-    uint32_t v1_low;
-    uint32_t high; /* v1 in high half, v0 in low half */
-} packed_edge;
-
-static inline int64_t get_v0_from_edge(const packed_edge* p) {
-    return (p->v0_low | ((int64_t)((int16_t)(p->high & 0xFFFF)) << 32));
-}
-
-static inline int64_t get_v1_from_edge(const packed_edge* p) {
-    return (p->v1_low | ((int64_t)((int16_t)(p->high >> 16)) << 32));
-}
-
-static inline void write_edge(packed_edge* p, int64_t v0, int64_t v1) {
-    p->v0_low = (uint32_t)v0;
-    p->v1_low = (uint32_t)v1;
-    p->high   = (uint32_t)(((v0 >> 32) & 0xFFFF) | (((v1 >> 32) & 0xFFFF) << 16));
-}
-
-#else
-
-typedef struct packed_edge {
-    int64_t v0;
-    int64_t v1;
-} packed_edge;
-
-static inline int64_t get_v0_from_edge(const packed_edge* p) {
-    return p->v0;
-}
-
-static inline int64_t get_v1_from_edge(const packed_edge* p) {
-    return p->v1;
-}
-
-static inline void write_edge(packed_edge* p, int64_t v0, int64_t v1) {
-    p->v0 = v0;
-    p->v1 = v1;
-}
-
-#endif
-
 Kronecker::Kronecker(const PGeneratorConfig& config, const PEID rank, const PEID size)
     : config_(config),
       size_(size),
