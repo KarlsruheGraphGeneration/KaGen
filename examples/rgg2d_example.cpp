7469548641bc988f3df5bd08ee99ff12f9189d47
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-07-07 18:27:19 +0200

    Example

---
+++ examples/rgg2d_example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+
+#include <kagen.h>
+
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    kagen::KaGen generator(MPI_COMM_WORLD);
+    const auto [edges, range] = generator.GenerateRGG2D_NM(16, 32);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    {
+        std::stringstream ss;
+        ss << "On PE " << rank << " [" << range.first << ", " << range.second << "): ";
+        for (const auto& [u, v]: edges) {
+            ss << u << "->" << v << " ";
+        }
+        ss << "\n";
+        std::cout << ss.str();
+    }
+
+    MPI_Finalize();
+}
