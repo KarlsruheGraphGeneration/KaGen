379232eb26bfd92c24be3e1afe1931a4f222b8a1
Daniel Seemaier <daniel@seemaier.de>
2023-05-25 16:44:39 +0200

    Allow multiple files with graphstats

--- app/tools/graphstats.cpp
+++ app/tools/graphstats.cpp
@@ -52,21 +52,17 @@ void print_csv_header() {
     std::cout << "MinDeg,";
     std::cout << "AvgDeg,";
     std::cout << "MaxDeg";
-    std::cout << "\n";
+    std::cout << std::endl;
 }
 
-void print_csv_row(const Statistics& stats, const bool print_header) {
-    if (print_header) {
-        print_csv_header();
-    }
-
+void print_csv_row(const Statistics& stats) {
     std::cout << stats.name << ",";
     std::cout << stats.n << ",";
     std::cout << stats.m << ",";
     std::cout << stats.min_deg << ",";
     std::cout << stats.avg_deg << ",";
     std::cout << stats.max_deg;
-    std::cout << "\n";
+    std::cout << std::endl;
 }
 
 std::string extract_filename(const std::string& filename) {
@@ -88,16 +84,17 @@ std::string strip_extension(const std::string& filename) {
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
-    bool             do_strip_extension = false;
-    bool             do_no_header       = false;
-    bool             do_header_only     = false;
-    PGeneratorConfig config;
+    std::vector<std::string> input_filenames;
+    bool                     do_strip_extension = false;
+    bool                     do_no_header       = false;
+    bool                     do_header_only     = false;
+    PGeneratorConfig         config;
 
     CLI::App app("graphstats: compute some basic statistics on a graph");
 
     CLI::Option_group* group = app.add_option_group("Options");
     group->require_option(1);
-    group->add_option("input filename", config.input_graph.filename)->check(CLI::ExistingFile);
+    group->add_option("input filenames", input_filenames)->check(CLI::ExistingFile);
     group->add_flag("--header-only", do_header_only);
 
     app.add_option("-f,--format", config.input_graph.format)->transform(CLI::CheckedTransformer(GetInputFormatMap()));
@@ -106,31 +103,33 @@ int main(int argc, char* argv[]) {
     CLI11_PARSE(app, argc, argv);
 
     // Catch special case: only print CSV header line
+    if ((do_header_only || !do_no_header) && get_rank() == ROOT) {
+        print_csv_header();
+    }
     if (do_header_only) {
-        if (get_rank() == ROOT) {
-            print_csv_header();
-        }
         return MPI_Finalize();
     }
 
-    // ... otherwise, continue with loading the graph
-    Graph graph = load_graph(config);
+    for (const auto& filename: input_filenames) {
+        config.input_graph.filename = filename;
+        Graph graph                 = load_graph(config);
 
-    Statistics stats;
-    stats.name = extract_filename(config.input_graph.filename);
-    if (do_strip_extension) {
-        stats.name = strip_extension(stats.name);
-    }
-    stats.n = FindNumberOfGlobalNodes(graph.vertex_range, MPI_COMM_WORLD);
-    stats.m = FindNumberOfGlobalEdges(graph.edges, MPI_COMM_WORLD);
+        Statistics stats;
+        stats.name = extract_filename(config.input_graph.filename);
+        if (do_strip_extension) {
+            stats.name = strip_extension(stats.name);
+        }
+        stats.n = FindNumberOfGlobalNodes(graph.vertex_range, MPI_COMM_WORLD);
+        stats.m = FindNumberOfGlobalEdges(graph.edges, MPI_COMM_WORLD);
 
-    const auto degree_stats = ReduceDegreeStatistics(graph.edges, stats.n, MPI_COMM_WORLD);
-    stats.min_deg           = degree_stats.min;
-    stats.avg_deg           = degree_stats.mean;
-    stats.max_deg           = degree_stats.max;
+        const auto degree_stats = ReduceDegreeStatistics(graph.edges, stats.n, MPI_COMM_WORLD);
+        stats.min_deg           = degree_stats.min;
+        stats.avg_deg           = degree_stats.mean;
+        stats.max_deg           = degree_stats.max;
 
-    if (get_rank() == ROOT) {
-        print_csv_row(stats, !do_no_header);
+        if (get_rank() == ROOT) {
+            print_csv_row(stats);
+        }
     }
 
     return MPI_Finalize();
