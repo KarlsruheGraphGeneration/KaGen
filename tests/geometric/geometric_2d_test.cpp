KarlsruheGraphGeneration/KaGen

commit 0dc313111ce5a17d41e9babbd67a7bdb3cb757f3

Add unit tests for small RGG2D and RGG3D graphs (#37)

kagen/generators/geometric/rgg.cpp
@@ -120,7 +120,6 @@ PGeneratorConfig NormalizeParametersCommon(
 PGeneratorConfig
 RGG2DFactory::NormalizeParameters(PGeneratorConfig config, PEID, const PEID size, const bool output) const {
     EnsureSquarePowerOfTwoChunkSize(config, size, output);
-    // EnsurePowerOfTwoCommunicatorSize(config, size);
 
     return NormalizeParametersCommon(config, &ApproxRadius2D, &ApproxNumNodes2D, output);
 }
@@ -132,8 +131,7 @@ RGG2DFactory::Create(const PGeneratorConfig& config, const PEID rank, const PEID
 
 PGeneratorConfig
 RGG3DFactory::NormalizeParameters(PGeneratorConfig config, PEID, const PEID size, const bool output) const {
-    EnsureCubicPowerOfTwoChunkSize(config, size, output);
-    //EnsurePowerOfTwoCommunicatorSize(config, size);
+    EnsureSquarePowerOfTwoChunkSize(config, size, output);
 
     return NormalizeParametersCommon(config, &ApproxRadius3D, &ApproxNumNodes3D, output);
 }

tests/CMakeLists.txt
@@ -42,5 +42,18 @@ kagen_add_test(test_generic_file_generator
     FILES file/generic_file_generator_test.cpp
     CORES 1 2 3 4 8 16)
 
+# Geometric Generator
+kagen_add_test(test_geometric_one_PE
+        FILES geometric/geometric_one_PE_test.cpp
+        CORES 1)
+        
+kagen_add_test(geometric_2d_test
+        FILES geometric/geometric_2d_test.cpp
+        CORES 1 2 4)
+
+kagen_add_test(geometric_3d_test
+        FILES geometric/geometric_3d_test.cpp
+        CORES 1 2 4)
+
 
 

tests/geometric/geometric_2d_test.cpp
@@ -1,0 +1,83 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+#include "kagen/context.h"
+#include "kagen/definitions.h"
+#include "kagen/generators/geometric/geometric_2d.h"
+#include "kagen/generators/geometric/rgg.h"
+#include "tests/util/utils.h"
+#include "geometric/rgg_utils.h"
+
+using namespace kagen;
+
+TEST(Geometric2DTest, generates_graph_on_np_PE_n32_r125) {
+    kagen::testing::f();
+
+    PGeneratorConfig config;
+    config.n           = 32;
+    config.r           = 0.125;
+    config.coordinates = true;
+
+    RGG2DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+
+    auto global_graph = kagen::testing::GatherCoordinates2D(result);
+
+    if (rank == 0) {
+        ASSERT_EQ(config.n, global_graph.coordinates.first.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG2DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        ASSERT_EQ(complete_graph.edges, edge_List);
+    }
+}
+
+TEST(Geometric2DTest, generates_graph_on_np_PE_n16_r10) {
+    PGeneratorConfig config;
+    config.n           = 16;
+    config.r           = 0.1;
+    config.coordinates = true;
+
+    RGG2DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+    auto  global_graph   = kagen::testing::GatherCoordinates2D(result);
+
+    if (rank == 0) {
+        ASSERT_EQ(config.n, global_graph.coordinates.first.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG2DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        ASSERT_EQ(complete_graph.edges, edge_List);
+    }
+}

tests/geometric/geometric_3d_test.cpp
@@ -1,0 +1,80 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+#include "kagen/context.h"
+#include "kagen/definitions.h"
+#include "kagen/generators/geometric/geometric_2d.h"
+#include "kagen/generators/geometric/rgg.h"
+#include "tests/util/utils.h"
+#include "geometric/rgg_utils.h"
+
+using namespace kagen;
+
+TEST(Geometric3DTest, generates_graph_on_np_PE_n32_r125) {
+    PGeneratorConfig config;
+    config.n           = 32;
+    config.r           = 0.125;
+    config.coordinates = true;
+
+    RGG3DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+    auto  global_graph   = kagen::testing::GatherCoordinates3D(result);
+
+    if (rank == 0) {
+        ASSERT_EQ(config.n, global_graph.coordinates.second.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG3DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        ASSERT_EQ(complete_graph.edges, edge_List);
+    }
+}
+
+TEST(Geometric3DTest, generates_graph_on_np_PE_n16_r10) {
+    PGeneratorConfig config;
+    config.n           = 16;
+    config.r           = 0.1;
+    config.coordinates = true;
+
+    RGG3DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+    auto  global_graph   = kagen::testing::GatherCoordinates3D(result);
+
+    if (rank == 0) {
+        ASSERT_EQ(config.n, global_graph.coordinates.second.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG3DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        ASSERT_EQ(complete_graph.edges, edge_List);
+    }
+}

kagen/generators/generator.cpp
@@ -161,7 +161,6 @@ void GeneratorFactory::EnsureSquarePowerOfTwoChunkSize(
             if (!IsSquare(config.k)) {
                 config.k *= 2;
             }
-
             while (std::ceil(1.0 * config.k / size) > (1.0 + config.max_vertex_imbalance) * config.k / size) {
                 config.k <<= 2;
             }

tests/geometric/geometric_one_PE_test.cpp
@@ -1,0 +1,62 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+#include "kagen/context.h"
+#include "kagen/definitions.h"
+#include "kagen/generators/geometric/geometric_2d.h"
+#include "kagen/generators/geometric/rgg.h"
+#include "tests/util/utils.h"
+#include "geometric/rgg_utils.h"
+
+using namespace kagen;
+
+TEST(Geometric1PETest, generates_2D_graph) {
+    PGeneratorConfig config;
+    config.n           = 32;
+    config.r           = 0.5;
+    config.coordinates = true;
+
+    RGG2DFactory factory;
+    config         = factory.NormalizeParameters(config, 0, 1, false);
+    auto generator = factory.Create(config, 0, 1);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    ASSERT_EQ(config.n, result.coordinates.first.size());
+
+    // Creating the correct edge list as a test instance
+    std::vector<std::pair<SInt, SInt>> edge_List = kagen::testing::CreateExpectedRGG2DEdges(config, result);
+
+    // Sorting both lists before comparing them
+    std::sort(result.edges.begin(), result.edges.end());
+    std::sort(edge_List.begin(), edge_List.end());
+
+    ASSERT_EQ(result.edges, edge_List);
+}
+
+TEST(Geometric1PETest, generates_3D_graph) {
+    PGeneratorConfig config;
+    config.n           = 32;
+    config.r           = 0.5;
+    config.coordinates = true;
+
+    RGG3DFactory factory;
+    config         = factory.NormalizeParameters(config, 0, 1, false);
+    auto generator = factory.Create(config, 0, 1);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    ASSERT_EQ(config.n, result.coordinates.second.size());
+
+    // Creating the correct edge list as a test instance
+    std::vector<std::pair<SInt, SInt>> edge_List = kagen::testing::CreateExpectedRGG3DEdges(config, result);
+
+    // Sorting both lists before comparing them
+    std::sort(result.edges.begin(), result.edges.end());
+    std::sort(edge_List.begin(), edge_List.end());
+
+    ASSERT_EQ(result.edges, edge_List);
+}

tests/geometric/rgg_utils.h
@@ -1,0 +1,49 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <mpi.h>
+
+#include <cmath>
+#include <numeric>
+#include <utility>
+
+#include "kagen/context.h"
+
+namespace kagen::testing {
+
+inline void f () {
+
+}
+
+inline Edgelist CreateExpectedRGG2DEdges(PGeneratorConfig config, const Graph& graph) {
+    std::vector<std::pair<SInt, SInt>> edge_List;
+    for (SInt i = 0; i < config.n; i++) {
+        auto [x1, y1] = graph.coordinates.first[i];
+        for (SInt j = 0; j < config.n; j++) {
+            auto [x2, y2] = graph.coordinates.first[j];
+            // Comparing all coordinates
+            if (i != j && std::hypot(x1 - x2, y1 - y2) <= config.r) {
+                edge_List.emplace_back(i, j);
+            }
+        }
+    }
+    return edge_List;
+}
+
+inline Edgelist CreateExpectedRGG3DEdges(PGeneratorConfig config, const Graph& graph) {
+    std::vector<std::pair<SInt, SInt>> edge_List;
+    for (SInt i = 0; i < config.n; i++) {
+        auto [x1, y1, z1] = graph.coordinates.second[i];
+        for (SInt j = 0; j < config.n; j++) {
+            auto [x2, y2, z2] = graph.coordinates.second[j];
+            // Comparing all coordinates
+            if (i != j && std::hypot(x1 - x2, y1 - y2, z1 - z2) < config.r) {
+                edge_List.emplace_back(i, j);
+            }
+        }
+    }
+    return edge_List;
+}
+
+} // namespace kagen::testing

tests/util/utils.h
@@ -4,6 +4,7 @@
 
 #include <mpi.h>
 
+#include <cmath>
 #include <numeric>
 #include <utility>
 
@@ -124,4 +125,105 @@ inline Graph GatherGraph(const Graph& local_graph) {
 
     __builtin_unreachable();
 }
+
+inline Graph GatherCoordinates2D(const Graph& local_graph) {
+    PEID size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Count the amount of coordinates per PE
+    std::vector<HPFloat> x_values;
+    std::vector<HPFloat> y_values;
+    for (const auto& coord: local_graph.coordinates.first) {
+        auto [x, y] = coord;
+        x_values.push_back(x);
+        y_values.push_back(y);
+    }
+
+    // Exchange amount of coordinates between PEs
+    int              localSize = x_values.size();
+    std::vector<int> sizes(size);
+    MPI_Allgather(&localSize, 1, MPI_INT, sizes.data(), 1, MPI_INT, MPI_COMM_WORLD);
+
+    // Gather amount of vertices
+    int totalSize = std::accumulate(sizes.begin(), sizes.end(), 0);
+
+    // Exchange coordinates
+    std::vector<int> displacements(size);
+    std::exclusive_scan(sizes.begin(), sizes.end(), displacements.begin(), 0);
+
+    Graph global_graph;
+    global_graph.representation = local_graph.representation;
+    global_graph.coordinates.first.resize(totalSize);
+
+    std::vector<HPFloat> recv_x_values(totalSize);
+    std::vector<HPFloat> recv_y_values(totalSize);
+
+    MPI_Allgatherv(
+        x_values.data(), localSize, KAGEN_MPI_HPFLOAT, recv_x_values.data(), sizes.data(), displacements.data(),
+        KAGEN_MPI_HPFLOAT, MPI_COMM_WORLD);
+    MPI_Allgatherv(
+        y_values.data(), localSize, KAGEN_MPI_HPFLOAT, recv_y_values.data(), sizes.data(), displacements.data(),
+        KAGEN_MPI_HPFLOAT, MPI_COMM_WORLD);
+
+    for (int i = 0; i < totalSize; i++) {
+        global_graph.coordinates.first[i] = {recv_x_values[i], recv_y_values[i]};
+    }
+
+    return global_graph;
+}
+
+inline Graph GatherCoordinates3D(const Graph& local_graph) {
+    PEID size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Count the amount of coordinates per PE
+    std::vector<HPFloat> x_values;
+    std::vector<HPFloat> y_values;
+    std::vector<HPFloat> z_values;
+    for (const auto& coord: local_graph.coordinates.second) {
+        auto [x, y, z] = coord;
+        x_values.push_back(x);
+        y_values.push_back(y);
+        z_values.push_back(z);
+    }
+
+    // Exchange amount of coordinates between PEs
+    int              localSize = x_values.size();
+    std::vector<int> sizes(size);
+    MPI_Allgather(&localSize, 1, MPI_INT, sizes.data(), 1, MPI_INT, MPI_COMM_WORLD);
+
+    // Gather amount of vertices
+    int totalSize = std::accumulate(sizes.begin(), sizes.end(), 0);
+
+    // Exchange coordinates
+    std::vector<int> displacements(size);
+    std::exclusive_scan(sizes.begin(), sizes.end(), displacements.begin(), 0);
+
+    Graph global_graph;
+    global_graph.representation = local_graph.representation;
+    global_graph.coordinates.second.resize(totalSize);
+
+    std::vector<HPFloat> recv_x_values(totalSize);
+    std::vector<HPFloat> recv_y_values(totalSize);
+    std::vector<HPFloat> recv_z_values(totalSize);
+
+    MPI_Allgatherv(
+        x_values.data(), localSize, KAGEN_MPI_HPFLOAT, recv_x_values.data(), sizes.data(), displacements.data(),
+        KAGEN_MPI_HPFLOAT, MPI_COMM_WORLD);
+    MPI_Allgatherv(
+        y_values.data(), localSize, KAGEN_MPI_HPFLOAT, recv_y_values.data(), sizes.data(), displacements.data(),
+        KAGEN_MPI_HPFLOAT, MPI_COMM_WORLD);
+    MPI_Allgatherv(
+        z_values.data(), localSize, KAGEN_MPI_HPFLOAT, recv_z_values.data(), sizes.data(), displacements.data(),
+        KAGEN_MPI_HPFLOAT, MPI_COMM_WORLD);
+
+    for (int i = 0; i < totalSize; i++) {
+        global_graph.coordinates.second[i] = {recv_x_values[i], recv_y_values[i], recv_z_values[i]};
+    }
+
+    return global_graph;
+}
+
 } // namespace kagen::testing

commit 93ecf2012f07ba6b9fcda0c060ad26d48f849db3

Fix RGG (#39)
* Ensure cubic power of two chunk size for RGG3D generator again.

This bug was introduced in 0dc3131.

* Change `ASSERT_EQ` to `EXPECT_EQ` in tests.

* Fix generation of coordinates for geometric generators.

* Additional tests for RGGs.

* Fix RGG coordinate generation.

kagen/generators/geometric/geometric_2d.h
@@ -52,6 +52,9 @@ protected:
             GenerateChunk(i);
 
         SetVertexRange(start_node_, start_node_ + num_nodes_);
+        if (config_.coordinates) {
+            CollectCoordinates();
+        }
     }
 
     // Config
@@ -214,7 +217,7 @@ protected:
         std::get<3>(chunk) = true;
     }
 
-    void GenerateVertices(const SInt chunk_id, const SInt cell_id, const bool push_coordinates) {
+    void GenerateVertices(const SInt chunk_id, const SInt cell_id, const bool /*push_coordinates*/) {
         // Lazily compute chunk
         if (chunks_.find(chunk_id) == end(chunks_))
             ComputeChunk(chunk_id);
@@ -249,12 +252,20 @@ protected:
             LPFloat y = mersenne.Random() * cell_size_ + start_y;
 
             cell_vertices.emplace_back(x, y, offset + i);
-            // fprintf(edge_file, "v %f %f\n", x, y);
-            if (push_coordinates && config_.coordinates) {
-                PushCoordinate(x, y);
-            }
         }
         std::get<3>(cell) = true;
+    }
+
+    inline void CollectCoordinates() {
+        for (SInt chunk_id = local_chunk_start_; chunk_id < local_chunk_end_; chunk_id++) {
+            for (SInt i = 0; i < cells_per_chunk_; ++i) {
+                auto& cell_vertices = vertices_[ComputeGlobalCellId(chunk_id, i)];
+                for (auto& vertex : cell_vertices) {
+                    auto& [x, y, id] = vertex;
+                    PushCoordinate(x, y);
+                }
+            }
+        }
     }
 
     void GenerateVertices(const SInt chunk_id, const SInt cell_id, std::vector<Vertex>& vertex_buffer) {

kagen/generators/geometric/geometric_3d.h
@@ -52,6 +52,9 @@ protected:
             GenerateChunk(i);
 
         SetVertexRange(start_node_, start_node_ + num_nodes_);
+        if (config_.coordinates) {
+            CollectCoordinates();
+        }
     }
 
     // Config
@@ -265,7 +268,7 @@ protected:
         std::get<4>(chunk) = true;
     }
 
-    void GenerateVertices(const SInt chunk_id, const SInt cell_id, const bool push_coordinates) {
+    void GenerateVertices(const SInt chunk_id, const SInt cell_id, const bool /*push_coordinates*/) {
         // Lazily compute chunk
         if (chunks_.find(chunk_id) == end(chunks_)) {
             ComputeChunk(chunk_id);
@@ -304,13 +307,20 @@ protected:
             LPFloat z = mersenne_.Random() * cell_size_ + start_z;
 
             cell_vertices.emplace_back(x, y, z, offset + i);
-            // fprintf(edge_file, "v %f %f\n", x, y);
-
-            if (push_coordinates && config_.coordinates) {
-                PushCoordinate(x, y, z);
-            }
         }
         std::get<4>(cell) = true;
+    }
+
+    inline void CollectCoordinates() {
+        for (SInt chunk_id = local_chunk_start_; chunk_id < local_chunk_end_; chunk_id++) {
+            for (SInt i = 0; i < cells_per_chunk_; ++i) {
+                auto& cell_vertices = vertices_[ComputeGlobalCellId(chunk_id, i)];
+                for (auto& vertex : cell_vertices) {
+                    auto& [x, y, z, id] = vertex;
+                    PushCoordinate(x, y, z);
+                }
+            }
+        }
     }
 
     void GenerateVertices(const SInt chunk_id, const SInt cell_id, std::vector<Vertex>& vertex_buffer) {

kagen/generators/geometric/rgg.cpp
@@ -131,7 +131,7 @@ RGG2DFactory::Create(const PGeneratorConfig& config, const PEID rank, const PEID
 
 PGeneratorConfig
 RGG3DFactory::NormalizeParameters(PGeneratorConfig config, PEID, const PEID size, const bool output) const {
-    EnsureSquarePowerOfTwoChunkSize(config, size, output);
+    EnsureCubicPowerOfTwoChunkSize(config, size, output);
 
     return NormalizeParametersCommon(config, &ApproxRadius3D, &ApproxNumNodes3D, output);
 }

kagen/generators/geometric/rgg/rgg_3d.cpp
@@ -93,8 +93,8 @@ void RGG3D::GenerateGridEdges(
     // Check if vertices not generated
     SInt first_global_cell_id  = ComputeGlobalCellId(first_chunk_id, first_cell_id);
     SInt second_global_cell_id = ComputeGlobalCellId(second_chunk_id, second_cell_id);
-    GenerateVertices(first_chunk_id, first_cell_id, false);
-    GenerateVertices(second_chunk_id, second_cell_id, false);
+    GenerateVertices(first_chunk_id, first_cell_id, true);
+    GenerateVertices(second_chunk_id, second_cell_id, true);
 
     // Gather vertices
     if (vertices_.find(first_global_cell_id) == end(vertices_))

tests/CMakeLists.txt
@@ -49,11 +49,8 @@ kagen_add_test(test_geometric_one_PE
         
 kagen_add_test(geometric_2d_test
         FILES geometric/geometric_2d_test.cpp
-        CORES 1 2 4)
+        CORES 1 2 3 4 5 6)
 
 kagen_add_test(geometric_3d_test
         FILES geometric/geometric_3d_test.cpp
-        CORES 1 2 4)
-
-
-
+        CORES 1 2 3 4 5 6)

tests/geometric/geometric_2d_test.cpp
@@ -81,3 +81,38 @@ TEST(Geometric2DTest, generates_graph_on_np_PE_n16_r10) {
         ASSERT_EQ(complete_graph.edges, edge_List);
     }
 }
+
+
+TEST(Geometric2DTest, generates_graph_on_np_PE_n512_r01) {
+    PGeneratorConfig config;
+    config.n           = 512;
+    config.r           = 0.01;
+    config.coordinates = true;
+
+    RGG2DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+    auto  global_graph   = kagen::testing::GatherCoordinates2D(result);
+
+    if (rank == 0) {
+        ASSERT_EQ(config.n, global_graph.coordinates.first.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG2DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        ASSERT_EQ(complete_graph.edges, edge_List);
+    }
+}

tests/geometric/geometric_3d_test.cpp
@@ -31,7 +31,7 @@ TEST(Geometric3DTest, generates_graph_on_np_PE_n32_r125) {
     auto  global_graph   = kagen::testing::GatherCoordinates3D(result);
 
     if (rank == 0) {
-        ASSERT_EQ(config.n, global_graph.coordinates.second.size());
+        EXPECT_EQ(config.n, global_graph.coordinates.second.size());
 
         // Creating the correct edge list as a test instance
         std::vector<std::pair<SInt, SInt>> edge_List =
@@ -41,7 +41,7 @@ TEST(Geometric3DTest, generates_graph_on_np_PE_n32_r125) {
         std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
         std::sort(edge_List.begin(), edge_List.end());
 
-        ASSERT_EQ(complete_graph.edges, edge_List);
+        EXPECT_EQ(complete_graph.edges, edge_List);
     }
 }
 
@@ -65,7 +65,7 @@ TEST(Geometric3DTest, generates_graph_on_np_PE_n16_r10) {
     auto  global_graph   = kagen::testing::GatherCoordinates3D(result);
 
     if (rank == 0) {
-        ASSERT_EQ(config.n, global_graph.coordinates.second.size());
+        EXPECT_EQ(config.n, global_graph.coordinates.second.size());
 
         // Creating the correct edge list as a test instance
         std::vector<std::pair<SInt, SInt>> edge_List =
@@ -75,6 +75,40 @@ TEST(Geometric3DTest, generates_graph_on_np_PE_n16_r10) {
         std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
         std::sort(edge_List.begin(), edge_List.end());
 
-        ASSERT_EQ(complete_graph.edges, edge_List);
+        EXPECT_EQ(complete_graph.edges, edge_List);
+    }
+}
+
+TEST(Geometric3DTest, generates_graph_on_np_PE_n512_r01) {
+    PGeneratorConfig config;
+    config.n           = 512;
+    config.r           = 0.01;
+    config.coordinates = true;
+
+    RGG3DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+    auto  global_graph   = kagen::testing::GatherCoordinates3D(result);
+
+    if (rank == 0) {
+        EXPECT_EQ(config.n, global_graph.coordinates.second.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG3DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        EXPECT_EQ(complete_graph.edges, edge_List);
     }
 }