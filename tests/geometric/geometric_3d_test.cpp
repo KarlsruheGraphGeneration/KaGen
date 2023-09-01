291c1499b4ca6c2a797936e7f9bbe1777b456a89
Tim Niklas Uhl <uhl@kit.edu>
2023-09-01 16:32:13 +0200

    Additional tests for RGGs.

--- tests/geometric/geometric_3d_test.cpp
+++ tests/geometric/geometric_3d_test.cpp
@@ -78,3 +78,37 @@ TEST(Geometric3DTest, generates_graph_on_np_PE_n16_r10) {
         EXPECT_EQ(complete_graph.edges, edge_List);
     }
 }
+
+TEST(Geometric3DTest, generates_graph_on_np_PE_n512_r01) {
+    PGeneratorConfig config;
+    config.n           = 512;
+    config.r           = 0.01;
+    config.coordinates = true;
+
+    RGG3DFactory factory;
+    PEID         size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    config         = factory.NormalizeParameters(config, rank, size, false);
+    auto generator = factory.Create(config, rank, size);
+    generator->Generate(GraphRepresentation::EDGE_LIST);
+    generator->Finalize(MPI_COMM_WORLD);
+    auto result = generator->Take();
+
+    Graph complete_graph = kagen::testing::GatherEdgeLists(result);
+    auto  global_graph   = kagen::testing::GatherCoordinates3D(result);
+
+    if (rank == 0) {
+        EXPECT_EQ(config.n, global_graph.coordinates.second.size());
+
+        // Creating the correct edge list as a test instance
+        std::vector<std::pair<SInt, SInt>> edge_List =
+            kagen::testing::CreateExpectedRGG3DEdges(config, global_graph);
+
+        // Sorting both lists before comparing them
+        std::sort(complete_graph.edges.begin(), complete_graph.edges.end());
+        std::sort(edge_List.begin(), edge_List.end());
+
+        EXPECT_EQ(complete_graph.edges, edge_List);
+    }
+}
