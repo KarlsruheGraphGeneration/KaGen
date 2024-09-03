92e5be15a4929c48463b2a79302bf9ed46b1d146
Daniel Seemaier <daniel+github@seemaier.de>
2024-09-03 17:05:00 +0200

    feat: dump edges individually in the streaming example

--- examples/streaming_example.cpp
+++ examples/streaming_example.cpp
@@ -13,15 +13,16 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (argc < 2) {
+    if (argc < 4) {
         if (rank == 0) {
-            std::cout << "Usage: ./streaming_example <graph> <chunks = 32>" << std::endl;
+            std::cout << "Usage: ./streaming_example <graph> <chunks = 32> <out-dir>" << std::endl;
         }
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    const std::string graph  = argv[1];
-    const kagen::PEID chunks = (argc < 3 ? 32 : std::atoi(argv[2]));
+    const std::string graph   = argv[1];
+    const kagen::PEID chunks  = std::atoi(argv[2]);
+    const std::string out_dir = argv[3];
 
     if (rank == 0) {
         std::cout << "Graph: " << graph << ", chunks: " << chunks << std::endl;
@@ -30,8 +31,7 @@ int main(int argc, char* argv[]) {
     kagen::sKaGen gen(graph, chunks, MPI_COMM_WORLD);
     gen.Initialize();
 
-    kagen::VertexRange my_expected_vertex_range       = gen.EstimateVertexRange();
-    bool               respects_esimated_vertex_range = true;
+    kagen::VertexRange my_expected_vertex_range = gen.EstimateVertexRange();
 
     if (rank == 0) {
         for (int pe = 0; pe < size; ++pe) {
@@ -42,18 +42,18 @@ int main(int argc, char* argv[]) {
         std::cout << "Generating " << std::flush;
     }
 
-    std::vector<kagen::SInt> local_edges;
+    std::ofstream out(out_dir + "/" + std::to_string(rank) + ".edges", std::ios_base::trunc);
+
     while (gen.Continue()) {
         const kagen::StreamedGraph graph = gen.Next();
 
+        std::vector<kagen::SInt> local_edges;
         graph.ForEachEdge([&](const auto from, const auto to) {
             local_edges.push_back(from);
             local_edges.push_back(to);
-
-            if (from < my_expected_vertex_range.first || from >= my_expected_vertex_range.second) {
-                respects_esimated_vertex_range = false;
-            }
         });
+        out.write(reinterpret_cast<const char*>(local_edges.data()), local_edges.size() * sizeof(kagen::SInt));
+        local_edges.clear();
 
         if (rank == 0) {
             std::cout << "." << std::flush;
@@ -65,35 +65,6 @@ int main(int argc, char* argv[]) {
         std::cout << "Waiting for other PEs ..." << std::endl;
     }
     MPI_Barrier(MPI_COMM_WORLD);
-
-    MPI_Allreduce(MPI_IN_PLACE, &respects_esimated_vertex_range, 1, MPI_C_BOOL, MPI_LAND, MPI_COMM_WORLD);
-    if (rank == 0 && !respects_esimated_vertex_range) {
-        std::cout << "Info: Some edges on some PEs are out of the estimated vertex range." << std::endl;
-    }
-
-    if (rank == 0) {
-        std::cout << "Dumping edges from root ..." << std::endl;
-    }
-
-    const int num_local_edges = static_cast<int>(local_edges.size());
-
-    std::vector<int> recvcounts(size);
-    std::vector<int> recvdispls(size);
-
-    MPI_Gather(&num_local_edges, 1, MPI_INT, recvcounts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
-    std::exclusive_scan(recvcounts.begin(), recvcounts.end(), recvdispls.begin(), 0);
-
-    std::vector<kagen::SInt> global_edges(recvdispls.back() + recvcounts.back());
-    MPI_Gatherv(
-        local_edges.data(), num_local_edges, KAGEN_MPI_SINT, global_edges.data(), recvcounts.data(), recvdispls.data(),
-        KAGEN_MPI_SINT, 0, MPI_COMM_WORLD);
-
-    if (rank == 0) {
-        std::ofstream out("dumped.plain-edgelist", std::ios_base::trunc);
-        for (std::size_t i = 0; i < global_edges.size(); i += 2) {
-            out << global_edges[i] << " " << global_edges[i + 1] << "\n";
-        }
-    }
 
     MPI_Finalize();
 }
