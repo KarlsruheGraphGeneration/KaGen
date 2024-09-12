96293eaa16cb0ebf74e6bb10dbd5903738c46a3f
Tim Niklas Uhl <uhl@kit.edu>
2024-09-12 10:33:34 +0200

    Also fix it for vertex weights.

--- kagen/generators/generator.cpp
+++ kagen/generators/generator.cpp
@@ -113,7 +113,7 @@ void Generator::GenerateVertexWeights(VertexWeightConfig weight_config, MPI_Comm
             vertex_weight_generator->GenerateVertexWeights(graph_.vertex_range, graph_.edges, graph_.vertex_weights);
             break;
         case GraphRepresentation::CSR:
-            if (!graph_.xadj.empty()) {
+            if (!graph_.edges.empty()) {
                 vertex_weight_generator->GenerateVertexWeights(
                     graph_.vertex_range, graph_.edges, graph_.vertex_weights);
             } else {
