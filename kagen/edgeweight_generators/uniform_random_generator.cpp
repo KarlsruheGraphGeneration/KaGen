fc0865dac029e01b3f27c4e5b7268808e036f0ae
Matthias Schimek <schimek@kit.edu>
2025-11-06 11:26:36 +0100

    unify implementation for different representations

--- kagen/edgeweight_generators/uniform_random_generator.cpp
+++ kagen/edgeweight_generators/uniform_random_generator.cpp
@@ -34,14 +34,6 @@ struct EdgeHasher {
     }
 };
 
-std::pair<SInt, SInt> get_canonical_order(SInt u, SInt v) {
-    if (u < v) {
-        return std::make_pair(u, v);
-    } else {
-        return std::make_pair(v, u);
-    }
-};
-
 std::pair<SInt, SInt> get_canonical_order(std::pair<SInt, SInt> const& edge) {
     auto u = std::get<0>(edge);
     auto v = std::get<1>(edge);
@@ -86,49 +78,22 @@ struct SrcDstEqual {
     }
 };
 
-} // namespace
-
-UniformRandomEdgeWeightGenerator::UniformRandomEdgeWeightGenerator(
-    EdgeWeightConfig config, MPI_Comm comm, VertexRange vertex_range)
-    : config_(config),
-      comm_(comm),
-      vertex_range_(vertex_range) {
-    if (config_.weight_range_begin >= config_.weight_range_end) {
-        throw std::runtime_error("Weight causes undefined behavior, need weight_range_begin < weight_range_end.");
-    }
-}
-
-// void UniformRandomEdgeWeightGenerator::GenerateEdgeWeights(
-//     // TODO add more native implementation
-//     const XadjArray& xadj, const AdjncyArray& adjncy, EdgeWeights& weights) {
-//     int size;
-//     MPI_Comm_size(comm_, &size);
-//     // if (size == 1) {
-//     //     GenerateEdgeWeightsForSingleRanks(xadj, adjncy, weights, config_);
-//     // } else {
-//     const auto edge_list = BuildEdgeListFromCSR(vertex_range_, xadj, adjncy);
-//     GenerateEdgeWeights(edge_list, weights);
-//     //}
-// }
-
-void UniformRandomEdgeWeightGenerator::GenerateEdgeWeights(
-    const XadjArray& xadj, const AdjncyArray& adjncy, EdgeWeights& weights) {
+void GenerateEdgeWeightsImpl(
+    EdgeRange edge_range, EdgeWeights& weights, const EdgeWeightConfig& config, VertexRange vertex_range,
+    MPI_Comm comm_) {
     PEID rank;
     MPI_Comm_rank(comm_, &rank);
     std::mt19937                         gen = get_random_generator(rank);
-    std::uniform_int_distribution<SSInt> weight_dist(config_.weight_range_begin, config_.weight_range_end - 1);
+    std::uniform_int_distribution<SSInt> weight_dist(config.weight_range_begin, config.weight_range_end - 1);
 
-    const auto ranges                  = AllgatherVertexRange(vertex_range_, comm_);
+    const auto ranges                  = AllgatherVertexRange(vertex_range, comm_);
     const auto& [local_from, local_to] = ranges[rank];
-    const auto num_local_edges         = adjncy.size();
-    const auto num_local_vertices      = vertex_range_.second - vertex_range_.first;
-    using Edge                         = Edgelist::value_type;
+    using Edge                         = typename EdgeRange::Edge;
 
     std::unordered_map<PEID, std::vector<EdgeData>> message_buffers;
     std::unordered_map<Edge, SInt, EdgeHasher>      edge_to_weight_map;
 
-    EdgeRange range(xadj, adjncy, vertex_range_);
-    for (const auto& edge: range) {
+    for (const auto& edge: edge_range) {
         if (is_canonically_ordered(edge)) {
             // auto edge = std::make_pair(u_global, v_global);
             auto it = edge_to_weight_map.find(edge);
@@ -143,29 +108,7 @@ void UniformRandomEdgeWeightGenerator::GenerateEdgeWeights(
             }
         }
     }
-    // for (std::size_t u_local = 0; u_local < num_local_vertices; ++u_local) {
-    //     const SInt        u_global = static_cast<SInt>(local_from + static_cast<SInt>(u_local));
-    //     const std::size_t start    = static_cast<std::size_t>(xadj[u_local]);
-    //     const std::size_t end      = static_cast<std::size_t>(xadj[u_local + 1]);
 
-    //    for (std::size_t offset = start; offset < end; ++offset) {
-    //        const SInt v_global = static_cast<SInt>(adjncy[offset]);
-    //        if (is_canonically_ordered(u_global, v_global)) {
-    //            auto edge = std::make_pair(u_global, v_global);
-    //            auto it   = edge_to_weight_map.find(edge);
-    //            if (it == edge_to_weight_map.end()) {
-    //                const SSInt weight = weight_dist(gen);
-    //                const SInt  head   = std::get<1>(edge);
-    //                edge_to_weight_map.emplace(edge, weight);
-    //                if ((head < local_from || head >= local_to)) {
-    //                    const SInt pe = static_cast<SInt>(FindPEInRange(head, ranges));
-    //                    message_buffers[pe].emplace_back(edge, weight);
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
-    // exchange messages
     MPI_Datatype edgedata_mpi_type;
     MPI_Type_contiguous(sizeof(EdgeData), MPI_BYTE, &edgedata_mpi_type);
     MPI_Type_commit(&edgedata_mpi_type);
@@ -179,88 +122,34 @@ void UniformRandomEdgeWeightGenerator::GenerateEdgeWeights(
         edge_to_weight_map.emplace(key, weight);
     }
     dump(std::move(recv_buf));
-    // weights.resize(num_local_edges);
-    // for (std::size_t u_local = 0; u_local < num_local_vertices; ++u_local) {
-    //     const SInt        u_global = static_cast<SInt>(local_from + static_cast<SInt>(u_local));
-    //     const std::size_t start    = static_cast<std::size_t>(xadj[u_local]);
-    //     const std::size_t end      = static_cast<std::size_t>(xadj[u_local + 1]);
-
-    //    for (std::size_t offset = start; offset < end; ++offset) {
-    //        const SInt v_global = static_cast<SInt>(adjncy[offset]);
-    //        auto       it       = edge_to_weight_map.find(get_canonical_order(u_global, v_global));
-
-    //        assert(it != edge_to_weight_map.end());
-    //        weights[offset] = it->second;
-    //    }
-    //}
-    weights.resize(num_local_edges);
-    for (std::size_t u_local = 0; u_local < num_local_vertices; ++u_local) {
-        const SInt        u_global = static_cast<SInt>(local_from + static_cast<SInt>(u_local));
-        const std::size_t start    = static_cast<std::size_t>(xadj[u_local]);
-        const std::size_t end      = static_cast<std::size_t>(xadj[u_local + 1]);
-
-        for (std::size_t offset = start; offset < end; ++offset) {
-            const SInt v_global = static_cast<SInt>(adjncy[offset]);
-            auto       it       = edge_to_weight_map.find(get_canonical_order(u_global, v_global));
-
-            assert(it != edge_to_weight_map.end());
-            weights[offset] = it->second;
-        }
+    weights.resize(edge_range.size());
+    for (auto it = edge_range.begin(); it != edge_range.end(); ++it) {
+        auto find_result_it = edge_to_weight_map.find(get_canonical_order(*it));
+        assert(find_result_it != edge_to_weight_map.end());
+        weights[it.edge_index()] = find_result_it->second;
     }
 }
 
+} // namespace
+
+UniformRandomEdgeWeightGenerator::UniformRandomEdgeWeightGenerator(
+    EdgeWeightConfig config, MPI_Comm comm, VertexRange vertex_range)
+    : config_(config),
+      comm_(comm),
+      vertex_range_(vertex_range) {
+    if (config_.weight_range_begin >= config_.weight_range_end) {
+        throw std::runtime_error("Weight causes undefined behavior, need weight_range_begin < weight_range_end.");
+    }
+}
+
+void UniformRandomEdgeWeightGenerator::GenerateEdgeWeights(
+    const XadjArray& xadj, const AdjncyArray& adjncy, EdgeWeights& weights) {
+    EdgeRange edge_range(xadj, adjncy, vertex_range_);
+    GenerateEdgeWeightsImpl(edge_range, weights, config_, vertex_range_, comm_);
+}
+
 void UniformRandomEdgeWeightGenerator::GenerateEdgeWeights(const Edgelist& edgelist, EdgeWeights& weights) {
-    using Edge = std::pair<SInt, SInt>;
-    static_assert(std::is_same_v<typename Edgelist::value_type, Edge>);
-
-    PEID rank;
-    MPI_Comm_rank(comm_, &rank);
-    std::mt19937                         gen = get_random_generator(rank);
-    std::uniform_int_distribution<SSInt> weight_dist(config_.weight_range_begin, config_.weight_range_end - 1);
-
-    const auto ranges                  = AllgatherVertexRange(vertex_range_, comm_);
-    const auto& [local_from, local_to] = ranges[rank];
-    std::unordered_map<PEID, std::vector<EdgeData>> message_buffers;
-    std::unordered_map<Edge, SInt, EdgeHasher>      edge_to_weight_map;
-
-    for (size_t i = 0; i < edgelist.size(); ++i) {
-        const auto& edge = edgelist[i];
-        if (is_canonically_ordered(edge)) {
-            auto it = edge_to_weight_map.find(edge);
-            if (it == edge_to_weight_map.end()) {
-                const SSInt weight = weight_dist(gen);
-                const SInt  head   = std::get<1>(edge);
-                edge_to_weight_map.emplace(edge, weight);
-                if ((head < local_from || head >= local_to)) {
-                    const SInt pe = static_cast<SInt>(FindPEInRange(head, ranges));
-                    message_buffers[pe].emplace_back(edge, weight);
-                }
-            }
-        }
-    }
-
-    // exchange messages
-    MPI_Datatype edgedata_mpi_type;
-    MPI_Type_contiguous(sizeof(EdgeData), MPI_BYTE, &edgedata_mpi_type);
-    MPI_Type_commit(&edgedata_mpi_type);
-    auto recv_buf = ExchangeMessageBuffers(std::move(message_buffers), edgedata_mpi_type, comm_);
-    MPI_Type_free(&edgedata_mpi_type);
-
-    // add received cut edges into edge_weight storage
-    for (const auto& [u, v, weight]: recv_buf) {
-        const auto key = std::make_pair(u, v);
-        assert(is_canonically_ordered(u, v));
-        edge_to_weight_map.emplace(key, weight);
-    }
-    dump(std::move(recv_buf));
-
-    // look up the actual weight
-    weights.resize(edgelist.size());
-    for (size_t i = 0; i < edgelist.size(); ++i) {
-        const auto& edge = edgelist[i];
-        auto        it   = edge_to_weight_map.find(get_canonical_order(edge));
-        assert(it != edge_to_weight_map.end());
-        weights[i] = it->second;
-    }
+    EdgeRange edge_range(edgelist);
+    GenerateEdgeWeightsImpl(edge_range, weights, config_, vertex_range_, comm_);
 }
 } // namespace kagen
