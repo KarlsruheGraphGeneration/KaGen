28d0a736b792370d3e1b6e601d657b70037988f4
Niklas Uhl <uhl@kit.edu>
2025-12-04 10:28:21 +0100

    Use spaces instead of tabs.

--- kagen/generators/geometric/geometric_2d.h
+++ kagen/generators/geometric/geometric_2d.h
@@ -207,7 +207,7 @@ protected:
         for (SInt i = 0; i < cells_per_chunk_; ++i) {
             seed                  = config_.seed + chunk_id * cells_per_chunk_ + i + total_chunks_ * cells_per_chunk_;
             SInt    h             = sampling::Spooky::hash(seed);
-	    // due to potential floating point inaccuracies clamp probability
+            // due to potential floating point inaccuracies clamp probability
             SInt    cell_vertices = rng_.GenerateBinomial(h, n, std::clamp(cell_area / total_area, 0.0, 1.0));
             LPFloat cell_start_x  = std::get<1>(chunk) + (i / cells_per_dim_) * cell_size_;
             LPFloat cell_start_y  = std::get<2>(chunk) + (i % cells_per_dim_) * cell_size_;
